17699d0068d0a73ca464e6cb788cefd3b0c584c2
Anurag <akanuragkumar4@gmail.com>
2024-11-25 11:50:02 +0530

    ..

--- backend/app.py
+++ backend/app.py
@@ -2832,8 +2832,8 @@ emailed_candidates = []
 # New code
 
 groups = [
-    {'start': 0, 'end': 400, 'config': 'Developer'},
-    {'start': 400, 'end': 788, 'config': 'Developer_1'},
+    # {'start': 0, 'end': 400, 'config': 'Developer'},
+    {'start': 0, 'end': 400, 'config': 'Developer_1'},
     {'start': 788, 'end': 802, 'config': 'Leadership'},
     {'start': 802, 'end': 986, 'config': 'HR'},
     {'start': 986, 'end': 1001, 'config': 'Account'}
